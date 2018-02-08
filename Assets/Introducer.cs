00beedeefb67a212d7d9e920b3ad2e9e78a6c5e6
Rob Meade <baldy.meade@gmail.com>
2018-02-08 11:42:57 +0000

    Project Initial Commit

---
+++ Assets/Introducer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hi, I am a road!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
